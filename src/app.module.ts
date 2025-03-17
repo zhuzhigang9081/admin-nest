4db9b95cfea57f29cb6e4b1fb1e50daefe513bd5
zhuzhigang <13383538433@163.com>
2025-03-17 22:20:11 +0800

    feat:连接数据库

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module'
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',//用户名
+      password: 'root',//密码
+      database: 'fs_admin', //数据库名
+      entities:[],//数据库对应的entity
+      synchronize:true,//是否自动同步实体文件,生产环境关闭
+      // connectorPackage:""
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

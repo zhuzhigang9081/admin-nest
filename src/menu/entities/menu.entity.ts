43d270b45f35d714bc837cc28212ed81d8a8a53d
zhuzhigang <13383538433@163.com>
2025-07-11 11:02:20 +0800

    fix:菜单表添加mate

--- src/menu/entities/menu.entity.ts
+++ src/menu/entities/menu.entity.ts
@@ -1,59 +1,75 @@
-import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 @Entity('menu')
 export class Menu {
-    @PrimaryGeneratedColumn()
-    id: number;
-    //标题
-    @Column({
-        length: 20,
-    })
-    title: string;
-    //排序
-    @Column()
-    order_num: number;
-    //父id
-    @Column({
-        nullable: true,
-    })
-    parent_id: number;
-    //菜单类型 1目录 2菜单 3按钮
-    @Column()
-    menu_type: number;
-    //菜单图标
-    @Column({
-        length: 50,
-        nullable: true
-    })
-    icon: string;
-    // 组件路径
-    @Column({
-        length: 50,
-        nullable: true
-    })
-    component: string;
-    //权限标识
-    @Column({
-        length: 50,
-        nullable: true
-    })
-    permission: string;
-    //路由
-    @Column({
-        length: 50,
-    })
-    path: string;
+  @PrimaryGeneratedColumn()
+  id: number;
+  //标题
+  @Column({
+    length: 20,
+  })
+  title: string;
+  //排序
+  @Column()
+  order_num: number;
+  //父id
+  @Column({
+    nullable: true,
+  })
+  parent_id: number;
+  //菜单类型 1目录 2菜单 3按钮
+  @Column()
+  menu_type: number;
+  //菜单图标
+  @Column({
+    length: 50,
+    nullable: true,
+  })
+  icon: string;
+  // 组件路径
+  @Column({
+    length: 50,
+    nullable: true,
+  })
+  component: string;
+  //权限标识
+  @Column({
+    length: 50,
+    nullable: true,
+  })
+  permission: string;
+  //路由
+  @Column({
+    length: 50,
+  })
+  path: string;
 
-    @Column({
-        type: 'bigint'
-    })
-    create_by: number;
-    //状态 1启用 0禁用
-    @Column({
-        default: 1
-    })
-    status: number;
-    @CreateDateColumn()
-    create_time: Date;
-    @UpdateDateColumn()
-    update_time: Date;
+  @Column({
+    type: 'bigint',
+  })
+  create_by: number;
+  //状态 1启用 0禁用
+  @Column({
+    default: 1,
+  })
+  status: number;
+  //是否隐藏侧边菜单 1:隐藏 0:显示
+  @Column({
+    default: 0,
+  })
+  hidden: number;
+  //状态 1:缓存 0:不缓存
+  @Column({
+    default: 0,
+  })
+  cache: number;
+  @CreateDateColumn()
+  create_time: Date;
+  @UpdateDateColumn()
+  update_time: Date;
 }
--- src/utils/convertToTree.ts
+++ src/utils/convertToTree.ts
@@ -1,16 +1,38 @@
-export const convertToTree = (menuList, parentId: number | null = null) => {
-    console.log(menuList, 'menuList****1')
-    const tree = [];
+import { Menu } from 'src/menu/entities/menu.entity';
+export interface TreeNode extends Menu {
+  name?: string;
+  meta?: {
+    title: string;
+    cache: number;
+    status: Boolean;
+    name: string;
+    hidden?: Boolean;
+  };
+  children?: TreeNode[];
+}
+export const convertToTree = (
+  menuList: TreeNode[],
+  parentId: number | null = null,
+) => {
+  const tree = [];
 
-    for (let i = 0; i < menuList.length; i++) {
-        if (menuList[i].parent_id === parentId) {
-            const children = convertToTree(menuList, menuList[i].id);
-            if (children.length) {
-                menuList[i].children = children;
-            }
-            tree.push(menuList[i]);
-        }
+  for (let i = 0; i < menuList.length; i++) {
+    if (!menuList[i].meta) {
+      menuList[i].meta = {
+        title: menuList[i].title,
+        cache: menuList[i].cache,
+        status: !menuList[i].status,
+        name: menuList[i].name,
+      };
     }
+    if (menuList[i].parent_id === parentId) {
+      const children = convertToTree(menuList, menuList[i].id);
+      if (children.length) {
+        menuList[i].children = children;
+      }
+      tree.push(menuList[i]);
+    }
+  }
 
-    return tree;
+  return tree;
 };
